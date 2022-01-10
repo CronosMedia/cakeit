c77642f58599fc88241a88ece107ec33048b6041
CronosMedia <cronosdesignro@gmail.com>
2022-01-10 04:30:55 +0200

    Modified archive

--- src/templates/archive.js
+++ src/templates/archive.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+
 import Layout from "../components/Layout/Layout"
 import BreadCrumb from "../components/BreadCrumb/BreadCrumb"
+import ArchiveSidebar from "../components/ArchiveSidebar/ArchiveSidebar"
 import Pagination from "../components/Pagination/Pagination"
 
 import {
@@ -13,56 +15,57 @@ import {
   StyledDate,
   StyledReadMore,
 } from "./archive.styles"
-import ArchiveSidebar from "../components/ArchiveSidebar/ArchiveSidebar"
 
 const archiveTemplate = ({
   data: { allWpPost },
   pageContext: { catId, catName, catUri, categories, numPages, currentPage },
-}) => {
-  const width = 1920
-  const height = 300
-  return (
-    <Layout>
-      <StaticImage
-        src="../images/archive-header.jpg"
-        placeholder="BLURRED"
-        width={width}
-        height={height}
-        alt="Archive hero image"
+}) => (
+  <Layout>
+    <StaticImage
+      src="../images/archive_headerimage.png"
+      placeholder="TRACED_SVG"
+      width={1920}
+      height={300}
+      alt="Archive Hero"
+    />
+    <Wrapper>
+      <BreadCrumb
+        parent={{
+          uri: "/blog/all-posts/",
+          title: "blog",
+        }}
       />
-      <Wrapper>
-        <BreadCrumb parent={{ uri: "/category/all-posts", title: "blog" }} />
-        <ContentWrapper>
-          <ArchiveSidebar catId={catId} categories={categories.edges} />
-          <PageContent>
-            <h1 dangerouslySetInnerHTML={{ __html: catName }} />
-            {allWpPost.edges.map(post => (
-              <article key={post.node.id} className="entry-content">
-                <Link to={`/blog${post.node.uri}`}>
-                  <StyledH2
-                    dangerouslySetInnerHTML={{ __html: post.node.title }}
-                  />
-                </Link>
-                <StyledDate
-                  dangerouslySetInnerHTML={{ __html: post.node.date }}
+      <ContentWrapper>
+        <ArchiveSidebar catId={catId} categories={categories.edges} />
+        <PageContent>
+          <h1 dangerouslySetInnerHTML={{ __html: catName }} />
+          {allWpPost.edges.map(post => (
+            <article key={post.node.id} className="entry-content">
+              <Link to={`/blog${post.node.uri}`}>
+                <StyledH2
+                  dangerouslySetInnerHTML={{ __html: post.node.title }}
                 />
-                <p dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
-                <StyledReadMore to={`/blog${post.node.uri}`}>
-                  Read More
-                </StyledReadMore>
-              </article>
-            ))}
-            <Pagination
-              catUri={catUri}
-              page={currentPage}
-              totalPages={numPages}
-            />
-          </PageContent>
-        </ContentWrapper>
-      </Wrapper>
-    </Layout>
-  )
-}
+              </Link>
+              <StyledDate
+                dangerouslySetInnerHTML={{ __html: post.node.date }}
+              />
+              <p dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
+              <StyledReadMore to={`/blog${post.node.uri}`}>
+                Read More
+              </StyledReadMore>
+              <div className="dot-divider" />
+            </article>
+          ))}
+          <Pagination
+            catUri={catUri}
+            page={currentPage}
+            totalPages={numPages}
+          />
+        </PageContent>
+      </ContentWrapper>
+    </Wrapper>
+  </Layout>
+)
 
 export default archiveTemplate
 
