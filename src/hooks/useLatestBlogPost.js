6cef118eb649b621656dc6d454c77a31a53a048f
CronosMedia <cronosdesignro@gmail.com>
2022-01-10 03:55:35 +0200

    sort Descending

--- src/hooks/useLatestBlogPost.js
+++ src/hooks/useLatestBlogPost.js
@@ -3,7 +3,7 @@ import { useStaticQuery, graphql } from "gatsby"
 export const useLatestBlogPost = () => {
   const data = useStaticQuery(graphql`
     query LatestBlogPostQuery {
-      allWpPost(sort: { fields: date, order: ASC }) {
+      allWpPost(sort: { fields: date, order: DESC }) {
         edges {
           node {
             title
